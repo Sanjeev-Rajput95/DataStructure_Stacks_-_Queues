0316fdfddbcd8b36084b113b59a738c1b3186bcc
sanjeev <sanjeevsingh.77d@gmail.com>
2022-11-11 00:59:59 +0530

    Putting element in queue

--- StackAndQueue/StackAndQueue/Program.cs
+++ StackAndQueue/StackAndQueue/Program.cs
@@ -4,21 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("====================== WELCOME TO STACK AND QUEUE PROGRAM ==========================");
 
-            Stack stack = new Stack();
-            stack.Push(70);
-            stack.Push(30);
-            stack.Push(56);
-            Console.WriteLine("-----------------");
-            stack.peak();
-            stack.Display();
-            Console.WriteLine("-----------------");
-            stack.pop();
-            Console.WriteLine("-----------------");
-            stack.peak();
-            stack.Display();
+            //Stack stack = new Stack();
+            //stack.Push(70);
+            //stack.Push(30);
+            //stack.Push(56);
+            //Console.WriteLine("-----------------");
+            //stack.peak();
+            //stack.Display();
+            //Console.WriteLine("-----------------");
+            //stack.pop();
+            //stack.peak();
+            //stack.Display();
             
+            Queue queue = new Queue();
+
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+            queue.display();
+            
+
+
+
 
         }
     }
